a6bbf86c96d7676dea8d530b605a7725505fe79d
Josh Haines <Josh@JoshHaines.com>
2021-11-16 10:06:39 -0500

    feat(endpoints): add metrics and status endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,3 +8,27 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+# Metrics Endpoint
+@app.route('/metrics', methods=["GET"])
+def metrics():
+    resp = dict(
+        UserCount = 140,
+        UserCountActive = 23
+    )
+    data = dict(
+        data= resp
+    )
+    if request.method == 'GET':
+        return jsonify(data)
+    
+# Status Endpoint
+@app.route('/status', methods=["GET"])
+def status():
+    resp = "OK - healthy"
+
+    data = dict(
+        result= resp
+    )
+    if request.method == 'GET':
+        return jsonify(data)
