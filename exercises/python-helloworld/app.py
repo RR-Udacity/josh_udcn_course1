272a404aae7a370d9d6d9bb68caee34f6bfe0848
Josh Haines <Josh@JoshHaines.com>
2021-11-16 12:06:23 -0500

    feat(logging): add logging per exercise

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,26 +1,29 @@
 from flask import Flask
 from flask import jsonify, request
+import json
+import logging
+import os
+
+# filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'app.log')
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
     return "Hello World!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
 
 # Metrics Endpoint
 @app.route('/metrics', methods=["GET"])
 def metrics():
-    resp = dict(
-        UserCount = 140,
-        UserCountActive = 23
-    )
-    data = dict(
-        data= resp
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 123}}),
+        status=200,
+        mimetype='application/json'
     )
     if request.method == 'GET':
-        return jsonify(data)
+        app.logger.info('Metrics request successful')
+        return response
     
 # Status Endpoint
 @app.route('/status', methods=["GET"])
@@ -31,4 +34,8 @@ def status():
         result= resp
     )
     if request.method == 'GET':
+        app.logger.info('Status request successful')
         return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
